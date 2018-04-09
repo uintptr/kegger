df50bb9221771fb9cc874f340dab6545cb0d2415
fsck <inaforloop@gmail.com>
2018-04-09 16:17:17 -0400

    scale: simplifies the arduino code. Just getting the raw values and they'll be interpreted in python. No need to push the logic all the way down to the microcontroller

---
+++ ino/src/scale.ino
@@ -0,0 +1,66 @@
+#include <DHT.h>
+#include <HX711.h>
+
+#define DHT_PIN                 2
+#define SCALE_DOUT              3
+#define SCALE_CLOCK             4
+
+#define LOOP_GRANULARITY_MS     1000
+
+static DHT      dht(DHT_PIN, DHT11);
+static HX711    scale(SCALE_DOUT, SCALE_CLOCK);
+
+static float    g_fTemperature;
+static float    g_fHumidity;
+static float    g_fWeight;
+
+void setup()
+{
+    Serial.begin (9600);
+
+    scale.set_scale();
+
+    g_fTemperature  = 0;
+    g_fHumidity     = 0;
+    g_fWeight       = 0;
+}
+
+void loop()
+{
+    Serial.println("D:Sampling temperature");
+    g_fTemperature  = dht.readTemperature();
+    g_fHumidity     = dht.readHumidity();
+
+    Serial.println("D:Sampling Scale");
+    scale.power_up();
+
+    g_fWeight = scale.read_average(20);
+
+    scale.power_down();
+
+    Serial.print("D:");
+    Serial.print(g_fTemperature);
+    Serial.print(",");
+    Serial.print(g_fHumidity);
+    Serial.print(",");
+    Serial.println(g_fWeight);
+
+    //
+    // Have to cast and compare against 0 because of NaN
+    //
+    if( 0 != ( int ) g_fTemperature      &&
+        0 != ( int ) g_fHumidity  &&
+        0 != ( int ) g_fWeight )
+    {
+        //
+        // CSV to make it easy to read off the serial line
+        //
+        Serial.print(g_fTemperature);
+        Serial.print(",");
+        Serial.print(g_fHumidity);
+        Serial.print(",");
+        Serial.println(g_fWeight);
+    }
+
+    delay(LOOP_GRANULARITY_MS);
+}
